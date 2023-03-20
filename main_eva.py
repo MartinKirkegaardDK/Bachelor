83a6aecd703c5e13a64681e32cb670e9e8d109a5
Eva Christelsdóttir <70897092+evachristelsdottir@users.noreply.github.com>
2023-03-20 16:06:34 +0100

    Logscale transformed

---
+++ main_eva.py
@@ -0,0 +1,9 @@
+#from utils.load import make_total_df
+#from src.PCA.correlation import top_abs_correlations
+#from src.Visualisations.dist_labels import dist_labels
+from src.Visualisations.dist_labels import logged_all
+
+
+#print(dist_labels("data/friendship_data/countries-countries-fb-social-connectedness-index-october-2021.tsv","DK",15))
+
+#print(top_abs_correlations(make_total_df(),10))
--- src/Visualisations/dist_labels.py
+++ src/Visualisations/dist_labels.py
@@ -5,9 +5,8 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
 
-def dist_labels(filepath, countrycode, number):
+def top_dist_labels(filepath, countrycode, number):
     # Read in the data
     df = pd.read_csv(filepath, delimiter="\t", keep_default_na=False)
 
@@ -44,4 +43,114 @@ def dist_labels(filepath, countrycode, number):
     # Show the plot
     plt.show()
 
-dist_labels("../../data/friendship_data/countries-countries-fb-social-connectedness-index-october-2021.tsv","DK",15)
+top_dist_labels("data/friendship_data/countries-countries-fb-social-connectedness-index-october-2021.tsv","DK",15)
+
+def logged_top_dist_labels(filepath, countrycode, number):
+     # Read in the data
+    df = pd.read_csv(filepath, delimiter="\t", keep_default_na=False)
+
+    # Defining the country of interest
+    country = countrycode
+
+    # Filter the data to only include relations where the country of interest is the user_loc
+    relations = df[df["user_loc"] == country]
+
+    # Filter out self-loops
+    relations = relations.loc[relations['fr_loc'] != country]
+
+    # Sort the relations by scaled_sci values and select the top 10 and bottom 10
+    top_relations = relations.nlargest(number, "scaled_sci")
+    bottom_relations = relations.nsmallest(number, "scaled_sci")
+
+    # Log-scale transforming the scaled_sci values
+    top_relations["scaled_sci"] = np.log10(top_relations["scaled_sci"])
+    bottom_relations["scaled_sci"] = np.log10(bottom_relations["scaled_sci"])
+
+    # Create subplots
+    fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+
+    # Plot the top 10 relations
+    axs[0].bar(top_relations["fr_loc"], top_relations["scaled_sci"])
+    axs[0].set_xlabel("Country")
+    axs[0].set_ylabel("Scaled Sci Value")
+    axs[0].set_title(f"Top {number} relations for {country}")
+    axs[0].tick_params(axis='x', labelrotation=90)
+
+    # Plot the bottom 10 relations
+    axs[1].bar(bottom_relations["fr_loc"], bottom_relations["scaled_sci"])
+    axs[1].set_xlabel("Country")
+    axs[1].set_ylabel("Scaled Sci Value")
+    axs[1].set_title(f"Bottom {number} relations for {country}")
+    axs[1].tick_params(axis='x', labelrotation=90)
+
+    # Show the plot
+    plt.show()
+
+logged_top_dist_labels("data/friendship_data/countries-countries-fb-social-connectedness-index-october-2021.tsv","DK",10)   
+
+def all(filepath, countrycode):
+     # Read in the data
+    df = pd.read_csv(filepath, delimiter="\t", keep_default_na=False)
+
+    # Defining the country of interest
+    country = countrycode
+
+    # Filter the data to only include relations where the country of interest is the user_loc
+    relations = df[df["user_loc"] == country]
+
+    # Filter out self-loops
+    relations = relations.loc[relations['fr_loc'] != country]
+
+    # Sort the relations by scaled_sci values in descending order
+    relations = relations.sort_values(by="scaled_sci", ascending=False)
+
+    # Create plot
+    fig, ax = plt.subplots(figsize=(14, 6))
+
+    # Plot the top 10 relations
+    ax.bar(relations["fr_loc"], relations["scaled_sci"])
+    ax.set_xlabel("Country")
+    ax.set_ylabel("Scaled Sci Value")
+    ax.set_title(f"Relations for {country} (not log-scale transformed)")
+    ax.tick_params(axis='x', labelrotation=90)
+
+    # Show the plot
+    plt.show()
+
+all("data/friendship_data/countries-countries-fb-social-connectedness-index-october-2021.tsv","DK") 
+
+def logged_all(filepath, countrycode):
+     # Read in the data
+    df = pd.read_csv(filepath, delimiter="\t", keep_default_na=False)
+
+    # Defining the country of interest
+    country = countrycode
+
+    # Filter the data to only include relations where the country of interest is the user_loc
+    relations = df[df["user_loc"] == country]
+
+    # Filter out self-loops
+    relations = relations.loc[relations['fr_loc'] != country]
+
+    # Sort the relations by scaled_sci values in descending order
+    relations = relations.sort_values(by="scaled_sci", ascending=False)
+
+    # Log-scale transforming the scaled_sci values
+    relations["scaled_sci"] = np.log10(relations["scaled_sci"])
+
+    # Create plot
+    fig, ax = plt.subplots(figsize=(14, 6))
+
+    # Plot the top 10 relations
+    ax.bar(relations["fr_loc"], relations["scaled_sci"])
+    ax.set_xlabel("Country")
+    ax.set_ylabel("Scaled Sci Value")
+    ax.set_title(f"Relations for {country} (log-scale transformed)")
+    ax.tick_params(axis='x', labelrotation=90)
+
+    # Show the plot
+    plt.show()
+
+logged_all("data/friendship_data/countries-countries-fb-social-connectedness-index-october-2021.tsv","DK")   
+
+
