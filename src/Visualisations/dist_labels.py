014cd3e2e3a29615d3bd40c2a642ea9b752ebfe5
Eva Christelsdóttir <70897092+evachristelsdottir@users.noreply.github.com>
2023-03-20 12:30:08 +0100

    Distribution over labels updated

--- src/Visualisations/dist_labels.py
+++ src/Visualisations/dist_labels.py
@@ -1,21 +1,32 @@
 import sys
 sys.path.append('..')
 
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 
-def dist_labels(file_path):
-    df = pd.read_csv(file_path, delimiter= "\t",keep_default_na=False)
+def dist_labels(filepath, countrycode):
+    # Read in the data
+    df = pd.read_csv(filepath, delimiter= "\t",keep_default_na=False)
 
-    # Standardize the data
+    # Defining the country of interest
+    country = countrycode
+
+    # Filtering the data to include the country of interest and all other countries
+    df_filtered = df.loc[((df["user_loc"] == country) & (df["fr_loc"] != country)) | ((df["user_loc"] != country) & (df["fr_loc"] == country))].copy()
+
+    # Calculating the standardization for the entire dataset
     scaler = StandardScaler()
-    df["scaled_sci"] = scaler.fit_transform(df[["scaled_sci"]])
+    df_filtered["scaled_sci"] = scaler.fit_transform(df_filtered[["scaled_sci"]])
 
-    # Plot the distribution of the scaled_sci column
-    sns.histplot(data=df, x="scaled_sci", kde=True)
+    # Take the log of the scaled_sci column
+    df_filtered["log_scaled_sci"] = np.log(df_filtered["scaled_sci"])
+
+    # Plot the distribution of log_scaled_sci for the country of interest as user_loc
+    sns.histplot(data=df_filtered, x="log_scaled_sci", kde=True)
+    plt.title(f"Distribution of log scaled_sci for {country} as user_loc")
     plt.show()
 
-
-dist_labels("../../data/friendship_data/countries-countries-fb-social-connectedness-index-october-2021.tsv")
+dist_labels("../../data/friendship_data/countries-countries-fb-social-connectedness-index-october-2021.tsv","SE")
