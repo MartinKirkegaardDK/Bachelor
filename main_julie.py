dbcb83ce675e400cb1ef1e7fc34f8bcbf5f2c3fd
Julie Hinge <78808112+juliehinge@users.noreply.github.com>
2023-03-27 13:13:54 +0200

    grid

--- main_julie.py
+++ main_julie.py
@@ -1,5 +1,7 @@
-from src.Visualisations.map.viz import Choropleth_map
+from src.Visualisations.distributions import std_dist_plots
 
 
 Choropleth_map('DE')
+categories = ["BusinessIndustry", "Education", "Uncategorized","FamilyRelationships","FitnessWellness","FoodDrink","HobbiesActivities","LifestyleCulture",'NewsEntertainment','NonLocalBus','People','ShoppingFashion','SportsOutdoors','Technology','TravelPlacesEvents']
 
+std_dist_plots("Cos",categories)
