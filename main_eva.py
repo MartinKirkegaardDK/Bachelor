487c3bf711e8882761a6b94ed6f0df52f7244c61
Eva Christelsdóttir <70897092+evachristelsdottir@users.noreply.github.com>
2023-03-29 13:33:24 +0200

    Changes

--- main_eva.py
+++ main_eva.py
@@ -1,11 +1,11 @@
 #from utils.load import make_total_df
 #from src.PCA.correlation import top_abs_correlations
-from src.Visualisations.dist_labels import dist_labels
+#from src.Visualisations.dist_labels import logged_all
 #from src.Visualisations.dist_labels import logged_all
 #from src.PCA.non_pos import check_non_positive_values
 #from src.PCA.check_links import check_link_values
 #from src.PCA.correlation import top_abs_correlations
-#from src.Visualisations.Continents.network import calculate_betweenness_centrality, draw_graph
+from src.Visualisations.Continents.network import calculate_betweenness_centrality, draw_graph
 
 #from src.Visualisations.distributions import std_dist_plots
 #print(dist_labels("data/friendship_data/countries-countries-fb-social-connectedness-index-october-2021.tsv","DK",15))
