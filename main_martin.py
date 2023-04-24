d2f34a3fba2db3c178022cc09316c15e50f8e216
Martin Kirkegaard <tjuppen100@gmail.com>
2023-04-24 10:13:11 +0200

    created pred vs label plots

--- main_martin.py
+++
@@ -1,8 +0,0 @@
-from src.examples.confidence_plot_d_tree_with_distance import run
-#run()
-#print("done 1")
-from src.examples.confidence_plot_continents_d_tree import run as run2
-run2()
-print("done 2")
-from src.examples.confidence_plot_d_tree import run as run3
-run3()
--- src/examples/gridsearch.py
+++ src/examples/gridsearch.py
@@ -15,4 +15,4 @@ def run():
                 'rf__max_features':[1.0], 'rf__n_estimators':[150], 'rf__max_depth':[None], 'rf__min_samples_split': [2], 'rf__min_samples_leaf': [2]
             }
 
-    gridsearch(pipe, param_grid = param_grid,with_distance= True)
+    gridsearch(pipe, param_grid = param_grid,with_distance= False)
